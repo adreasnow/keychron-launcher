deb3214515b3ac25781a9b8b56543bcfb49a1fb8
adreasnow <4799450+adreasnow@users.noreply.github.com>
2026-01-06 03:46:26 +0000

    [AUTOMATED] Update package.nix for tag 2026.1.2

--- package.nix
+++ package.nix
@@ -7,7 +7,7 @@
 let
   pname = "keychron-launcher";
   appName = "Keychron Launcher";
-  version = "2026.1.1";
+  version = "2026.1.2";
   meta = {
     description = "Keychron Launcher electron app";
     maintainers = with lib.maintainers; [
@@ -19,7 +19,7 @@ let
     owner = "adreasnow";
     repo = "keychron-launcher";
     tag = "v${version}";
-    hash = "sha256-c3uBZeBb0Zo+QQhxVcMPFMe2hIkiA3lnOyaR1T0qZM8=";
+    hash = "sha256-/bthepjRTP80eB0WjMf1ne2xFY88RRm6+8wmqjqwPZI=";
   };
 
   darwin = stdenv.mkDerivation {
