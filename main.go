bf800e7c0d1edf9263d6635b1cf2581432862e49
adreasnow <adrea.snow@gmail.com>
2026-03-22 16:04:15 +1100

    Implement app launcher functionality
    Add command execution to launch Keychron Launcher application.
    Remove redundant binaries configuration from Homebrew cask.

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("open", "-a", "Keychron Launcher")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
